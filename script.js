5fb1ae45f52a8759113095b56fcf3974255aec7b
Foysal <hasanfoysal505@gmail.com>
2025-04-22 18:04:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ createBtn.addEventListener("click",()=>{
     let img = document.createElement("img");
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable",true);
-    img.src = "/img/delete_12319540.png";
+    img.src = "delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
     
 
